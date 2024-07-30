49c43cf5b5f6ed1caef84b61f6454cc88a680238
Amannnnnn06 <amannnnnn1405@gmail.com>
2024-07-30 19:23:46 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,31 @@
+// Aman prajapati 23070123017
+// calculator
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+     float a,b,sum,multiply,sub;
+     float div;
+      cout<<"enter a";
+      cin>>a;
+     
+     cout<<"enter b";
+     cin>>b;
+      sum=a+b;
+      sub=a-b;
+      multiply=a*b;
+      div=a/b;
+       cout<<"sum is:"<<sum<<endl;
+       cout<<"subtraction is:"<<sub<<endl;
+       cout<<"division is:"<<div<<endl;
+       cout<<"product is:"<<multiply<<endl;
+       return 0;
+       
+ }
+//output
+ // enter a45
+//enter b 67
+//sum is:112
+//subtraction is:-22
+//division is:0.671642
+//product is:3015
