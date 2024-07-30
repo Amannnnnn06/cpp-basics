1e0825066e402a5bcd72050c1a70701a5c0656b1
Amannnnnn06 <amannnnnn1405@gmail.com>
2024-07-30 19:21:42 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+//Name:Aman prajapati
+//PRN:23070123017
+//Aim:Printing Hello World
+
+#include <iostream>
+using namespace std;
+
+int main() { 
+    
+    cout << "Hello World";
+   
+    return 0;
+} 
+
+
+//Output:
+//Hello World
